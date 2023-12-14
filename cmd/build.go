e86beea9b57bd9ad3d24ac7954649b43759fdffc
simon.reichenbach <simon.reichenbach@informaticon.com>
2023-12-14 19:17:28 +0100

    feat: add self registrating

---
+++ cmd/build.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"time"
+
+	"github.com/informaticon/dev.win.base.pbmanager/utils"
+	pborca "github.com/informaticon/lib.go.base.pborca"
+	"github.com/spf13/cobra"
+)
+
+// buildCmd represents the build command
+var buildCmd = &cobra.Command{
+	Use:   "build [options] <pbt path>",
+	Short: "Builds a PowerBuilder target",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		pbtFilePath := args[0]
+		if !filepath.IsAbs(pbtFilePath) {
+			pbtFilePath = filepath.Join(basePath, pbtFilePath)
+		}
+		if !utils.FileExists(pbtFilePath) || filepath.Ext(pbtFilePath) != ".pbt" {
+			return fmt.Errorf("file %s does not exist or is not a pbt file", pbtFilePath)
+		}
+
+		if orcaVars.pbVersion != 22 {
+			return fmt.Errorf("currently, only PowerBuilder 22 is supported")
+		}
+		var opts []func(*pborca.Orca)
+		opts = append(opts, pborca.WithOrcaTimeout(time.Duration(orcaVars.timeoutSeconds)*time.Second))
+		if orcaVars.serverAddr != "" {
+			opts = append(opts, pborca.WithOrcaServer(orcaVars.serverAddr, orcaVars.serverApiKey))
+		}
+		Orca, err := pborca.NewOrca(orcaVars.pbVersion, opts...)
+		if err != nil {
+			return err
+		}
+		logs, err := Orca.FullBuildTarget(pbtFilePath)
+		if len(logs) > 0 {
+			log.Printf("Compiler Log:\n%v\n", logs)
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Println("Build done")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(buildCmd)
+}
---
+++ cmd/config.go
@@ -0,0 +1,63 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/sys/windows/registry"
+)
+
+var registerSvnDiff bool
+var registerSvnMerge bool
+
+// configCmd represents the build command
+var configCmd = &cobra.Command{
+	Use:   "config [options]",
+	Short: "Sets config values",
+	Args:  cobra.ExactArgs(0),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		exePath, err := os.Executable()
+		if err != nil {
+			return fmt.Errorf("could not retrieve absolute path of executable starting this process: %v", err)
+		}
+
+		if registerSvnMerge {
+			err := installDiffMerge(`Software\TortoiseSVN\MergeTools`, exePath+` diff %base %mine %theirs %merged %fname --base-name %bname --mine-name %yname --theirs-name %tname`)
+			if err != nil {
+				return err
+			}
+		}
+		if registerSvnDiff {
+			err := installDiffMerge(`Software\TortoiseSVN\DiffTools`, exePath+` diff %base %mine --base-name %bname --mine-name %yname`)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	},
+}
+
+func init() {
+	configCmd.Flags().BoolVarP(&registerSvnDiff, "register-svn-diff", "d", false, "Register pbmanager as Diff tool for PBL files in TortoiseSVN")
+	configCmd.Flags().BoolVarP(&registerSvnMerge, "register-svn-merge", "m", false, "Register pbmanager as Merge tool for PBL files in TortoiseSVN")
+	rootCmd.AddCommand(configCmd)
+}
+
+func installDiffMerge(parentKey, commandLine string) error {
+	k, err := registry.OpenKey(registry.CURRENT_USER, parentKey,
+		registry.QUERY_VALUE|registry.SET_VALUE)
+	if err != nil {
+		return fmt.Errorf("could not open registry key of TortoiseSVN: %v", err)
+	}
+	defer k.Close()
+
+	err = k.SetStringValue(".pbl", commandLine)
+	if err != nil {
+		return fmt.Errorf("could not add new diff/merge) command: %v", err)
+	}
+	return nil
+}

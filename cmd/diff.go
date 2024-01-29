959e82225851f64ad4739b638614a3c21a97ed52
Raffaele Tani <raffaele.tani@informaticon.com>
2024-01-29 08:54:10 +0100

    feat: pbt diff

--- cmd/diff.go
+++ cmd/diff.go
@@ -9,9 +9,11 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 
 	pborca "github.com/informaticon/lib.go.base.pborca"
+	"github.com/informaticon/lib.go.base.pborca/orca"
 	"github.com/spf13/cobra"
 )
 
@@ -19,6 +21,11 @@ var mergeTool string
 var nameBase string
 var nameMine string
 var nameTheirs string
+
+type exportJob struct {
+	libraryPath     string
+	destinationPath string
+}
 
 // https://tortoisesvn.net/docs/release/TortoiseSVN_en/tsvn-dug-settings.html
 
@@ -52,11 +59,11 @@ var diffCmd = &cobra.Command{
 		}
 		defer Orca.Close()
 
-		pblFilePathBase, err := getCleanPblFilePath(basePath, args[0])
+		pblFilePathBase, err := getCleanPblPbtFilePath(basePath, args[0])
 		if err != nil {
 			return err
 		}
-		pblFilePathMine, err := getCleanPblFilePath(basePath, args[1])
+		pblFilePathMine, err := getCleanPblPbtFilePath(basePath, args[1])
 		if err != nil {
 			return err
 		}
@@ -65,45 +72,113 @@ var diffCmd = &cobra.Command{
 			if err != nil {
 				fmt.Println(err)
 			}
+			return nil
 		}
 
-		pblFilePathTheirs, err := getCleanPblFilePath(basePath, args[2])
-		if err != nil {
-			return err
-		}
-		var pblFilePathMerged string
-		if len(args) >= 4 {
-			pblFilePathMerged, err = getCleanPblFilePath(basePath, args[3])
+		if len(args) > 2 {
+			pblFilePathTheirs, err := getCleanPblPbtFilePath(basePath, args[2])
 			if err != nil {
 				return err
 			}
-		}
-		merge(Orca, pblFilePathBase, pblFilePathMine, pblFilePathTheirs, pblFilePathMerged)
 
-		return err
+			var pblFilePathMerged string
+			if len(args) >= 4 {
+				pblFilePathMerged, err = getCleanPblPbtFilePath(basePath, args[3])
+				if err != nil {
+					return err
+				}
+			}
+			if true == false {
+				merge(Orca, pblFilePathBase, pblFilePathMine, pblFilePathTheirs, pblFilePathMerged)
+			}
+
+			return err
+		}
+		return nil
 	},
 }
 
 func diff(Orca *pborca.Orca, objFilePathBase, objFilePathMine string) error {
-	tempDir := filepath.Join(os.TempDir(), "pbdiff", time.Now().Format("20170907_170606"))
+	tempDir := filepath.Join(os.TempDir(), "pbdiff", time.Now().Format("20060102_150405"))
 	os.MkdirAll(tempDir, 0664)
 	defer os.RemoveAll(tempDir)
-
 	objSrcPathBase := filepath.Join(tempDir, fmt.Sprintf("%s (%s)", filepath.Base(objFilePathBase), getPblFileDescr(objFilePathBase)))
 	os.MkdirAll(objSrcPathBase, 0664)
-	err := exportPbl(Orca, objFilePathBase, objSrcPathBase, true)
-	if err != nil {
-		return err
-	}
 	objSrcPathMine := filepath.Join(tempDir, fmt.Sprintf("%s (%s)", filepath.Base(objFilePathMine), getPblFileDescr(objFilePathMine)))
 	os.MkdirAll(objSrcPathMine, 0664)
-	err = exportPbl(Orca, objFilePathMine, objSrcPathMine, true)
-	if err != nil {
-		return err
+
+	c := make(chan exportJob)
+
+	if filepath.Ext(objFilePathBase) == ".pbt" {
+		//producer for pbt
+		go func() {
+			pbt, _ := orca.NewPbtFromFile(objFilePathBase)
+			for _, lib := range pbt.LibList {
+				var job exportJob
+				job.libraryPath = lib
+				job.destinationPath = objSrcPathBase
+				c <- job
+			}
+			pbt, _ = orca.NewPbtFromFile(objFilePathMine)
+			for _, lib := range pbt.LibList {
+				var job exportJob
+				job.libraryPath = lib
+				job.destinationPath = objSrcPathMine
+				c <- job
+			}
+			close(c)
+		}()
+	} else if filepath.Ext(objFilePathBase) == ".pbl" {
+		//producer for pbt
+		go func() {
+
+			var job exportJob
+			job.libraryPath = objFilePathBase
+			job.destinationPath = objSrcPathBase
+			c <- job
+
+			job.libraryPath = objFilePathMine
+			job.destinationPath = objSrcPathMine
+			c <- job
+
+			close(c)
+		}()
 	}
 
-	command := exec.Command(mergeTool, "/r", "/x", "/u", "/ignoreblanklines", objSrcPathMine, objSrcPathBase, "/dl", nameMine, "/dr", nameBase)
+	//consumer
+	numOfConsumers := 4
+	var wg1 sync.WaitGroup
+	for i := 1; i <= numOfConsumers; i++ {
+		wg1.Add(1)
+		go func() {
+			defer wg1.Done()
+			var opts []func(*pborca.Orca)
+			opts = append(opts, pborca.WithOrcaTimeout(time.Duration(orcaVars.timeoutSeconds)*time.Second))
+			opts = append(opts, pborca.WithMessageCallback(func(level uint32, msg string) {
+				log.Printf("%d: %s\n", level, msg)
+			}))
+			if orcaVars.serverAddr != "" {
+				opts = append(opts, pborca.WithOrcaServer(orcaVars.serverAddr, orcaVars.serverApiKey))
+			}
+			Orca, err := pborca.NewOrca(orcaVars.pbVersion, opts...)
 
+			if err != nil {
+				panic(err)
+			}
+
+			for job := range c {
+				fmt.Println("Exporting ", job.libraryPath, " to ", job.destinationPath)
+				err := exportPbl(Orca, job.libraryPath, job.destinationPath, true)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			Orca.Close()
+		}()
+	}
+
+	wg1.Wait()
+	command := exec.Command(mergeTool, "/r", "/x", "/u", "/ignoreblanklines", objSrcPathMine, objSrcPathBase, "/dl", nameMine, "/dr", nameBase)
 	out, err := command.CombinedOutput()
 	if err != nil {
 		return err

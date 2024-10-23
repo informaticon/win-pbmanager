99250810001deb036be9e4ce3a78724a107b5ee1
Simon Jutzi <simon.jutzi@informaticon.com>
2024-10-23 19:16:04 +0200

    feat: add delete function

---
+++ cmd/delete.go
@@ -0,0 +1,107 @@
+package cmd
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/informaticon/dev.win.base.pbmanager/utils"
+	pborca "github.com/informaticon/lib.go.base.pborca"
+	"github.com/spf13/cobra"
+)
+
+// deleteCmd represents the export command
+var deleteCmd = &cobra.Command{
+	Use:   "delete <pbl path> -n <object name>",
+	Short: "Removes an object from a pbl file",
+	Long:  `You can define regex patterns or "*" for <object name> `,
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		pblFilePath := args[0]
+		objName, _ := cmd.Flags().GetString("object-name")
+		fileType := filepath.Ext(pblFilePath)
+
+		// check/create obj regex
+		if objName == "*" || objName == "" {
+			objName = "^.*$"
+		}
+		if !strings.HasSuffix(objName, "$") {
+			objName += "$"
+		}
+		if !strings.HasPrefix(objName, "^") {
+			objName = "^" + objName
+		}
+		objRegex, err := regexp.Compile(objName)
+		if err != nil {
+			return err
+		}
+
+		if !filepath.IsAbs(pblFilePath) {
+			pblFilePath = filepath.Join(basePath, pblFilePath)
+		}
+		//check if provided pblFilePath exists and is allowed
+		if !utils.FileExists(pblFilePath) || fileType != ".pbl" {
+			return fmt.Errorf("file %s does not exist or is not a pbl file", pblFilePath)
+		}
+
+		if orcaVars.pbVersion != 22 {
+			return fmt.Errorf("currently, only PowerBuilder 22 is supported")
+		}
+		var opts []func(*pborca.Orca)
+		opts = append(opts, pborca.WithOrcaTimeout(time.Duration(orcaVars.timeoutSeconds)*time.Second))
+		if orcaVars.serverAddr != "" {
+			opts = append(opts, pborca.WithOrcaServer(orcaVars.serverAddr, orcaVars.serverApiKey))
+		}
+		Orca, err := pborca.NewOrca(orcaVars.pbVersion, opts...)
+		if err != nil {
+			return err
+		}
+		defer Orca.Close()
+
+		return deletePbl(Orca, pblFilePath, objRegex)
+	},
+}
+
+var ignoreMissing bool
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+	deleteCmd.PersistentFlags().StringP("object-name", "n", "*", "name or regex of object to export like 'inf1_u_mail.sru' or 'u_.*'")
+
+	deleteCmd.PersistentFlags().BoolVarP(&ignoreMissing, "ignore-missing", "i", true, "do not fail if object does not exist")
+}
+
+func deletePbl(Orca *pborca.Orca, pblFilePath string, objRegex *regexp.Regexp) error {
+	if ignoreMissing {
+		//outDir = filepath.Join(outDir, filepath.Base(pblFilePath))
+	}
+
+	objs, err := Orca.GetObjList(pblFilePath)
+	if err != nil {
+		return err
+	}
+
+	deletedCount := 0
+	for _, objArr := range objs {
+		for _, obj := range objArr.GetObjArr() {
+			objName := obj.GetName() + pborca.GetObjSuffixFromType(obj.GetObjType())
+			if objRegex.FindString(objName) == "" {
+				continue
+			}
+
+			err = Orca.DeleteObj(pblFilePath, objName)
+			if err != nil {
+				return err
+			}
+			deletedCount++
+			fmt.Printf("deleted %s\n", objName)
+		}
+	}
+	if deletedCount == 0 && !ignoreMissing {
+		return fmt.Errorf("no object matching '%s' found in %s", objRegex, pblFilePath)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,21 +1,23 @@
 module github.com/informaticon/dev.win.base.pbmanager
 
-go 1.21.1
+go 1.22.0
+
+toolchain go1.23.1
 
 require (
-	github.com/informaticon/lib.go.base.pborca v0.0.0-20240303132317-f370805179ca
-	github.com/spf13/cobra v1.8.0
-	golang.org/x/exp v0.0.0-20240409090435-93d18d7e34b8
-	golang.org/x/sys v0.19.0
-	golang.org/x/text v0.14.0
+	github.com/informaticon/lib.go.base.pborca v0.0.0-20241021204435-c9e1cd3a985f
+	github.com/spf13/cobra v1.8.1
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c
+	golang.org/x/sys v0.26.0
+	golang.org/x/text v0.19.0
 )
 
 require (
-	github.com/dlclark/regexp2 v1.11.0 // indirect
+	github.com/dlclark/regexp2 v1.11.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.24.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
-	google.golang.org/grpc v1.63.2 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241021214115-324edc3d5d38 // indirect
+	google.golang.org/grpc v1.67.1 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
 )
